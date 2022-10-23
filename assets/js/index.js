429afe333c0db3fc711d4be2ad981f2403478ca7
xl <1558655926@qq.com>
2022-10-23 16:51:25 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,13 +30,7 @@ function getUserInfo() {
             }
             renderAvater(res.data)
         },
-        //不论成功还是失败都会调用的函数
-        complete: function(res) {
-            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-                localStorage.removeItem('token')
-                location.href = '/login.html'
-            }
-        }
+
     })
 }
 
